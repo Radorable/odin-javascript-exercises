ab2561eb1d93c6956cb130c89ff3370b138c3f4f
Radorable <235507184+Radorable@users.noreply.github.com>
2026-05-13 09:51:09 +0200

    06_repeatString done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let add = "";
+    for (let i = 0; i < num; i++) {
+        add += string;
+    }
+    return add;
 };
 
 // Do not edit below this line
