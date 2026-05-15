0bfd9444f80ec2decef26b11837361e4ef043289
Radorable <235507184+Radorable@users.noreply.github.com>
2026-05-15 17:48:04 +0200

    08_removeFromArray done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rmv) {
+    for (let i = 0; i < rmv.length; i++) {
+        if (arr.includes(rmv[i])) {
+            arr.splice(arr.indexOf(rmv[i]), 1);
+            if (arr.includes(rmv[i])) {
+            i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
