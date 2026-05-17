d5025483c32b826be4f6e5683cd3844ba27121c4
Radorable <235507184+Radorable@users.noreply.github.com>
2026-05-17 15:14:48 +0200

    exercice 09_sumAll done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    } else {
+        let total = 0;
+    if (a < b) {
+        for (a; a <= b; a++) {
+            console.log(total += a);
+        }
+    } else {
+        for (b; b <= a; b++) {
+            console.log(total += b);
+        }
+    }
+        return total;   
+    }
+       
 };
 
 // Do not edit below this line
