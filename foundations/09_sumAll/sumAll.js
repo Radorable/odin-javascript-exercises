19156d22936e23f81826c03d8d40457b7ece0095
Radorable <235507184+Radorable@users.noreply.github.com>
2026-05-17 15:43:07 +0200

    optimisation

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,20 +1,13 @@
 const sumAll = function(a, b) {
-    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
-        return 'ERROR';
-    } else {
-        let total = 0;
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    if (a > b) [a, b] = [b, a];
+    let total = 0;
     if (a < b) {
         for (a; a <= b; a++) {
             console.log(total += a);
         }
-    } else {
-        for (b; b <= a; b++) {
-            console.log(total += b);
-        }
-    }
-        return total;   
-    }
-       
+        return total;
+   }    
 };
 
 // Do not edit below this line
