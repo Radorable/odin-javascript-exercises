bd300e249c7462c39cd67ea3c20a82babcca2030
Radorable <235507184+Radorable@users.noreply.github.com>
2026-05-19 15:17:04 +0200

    exercice 11_tempConversion done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+    const converted = (number - 32) * 5 / 9;
+    const result = Math.round(converted * 10) / 10
+    return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+    const converted = (number * 9 / 5) + 32;
+    const result = Math.round(converted * 10) / 10
+    return result;
 };
 
 // Do not edit below this line
